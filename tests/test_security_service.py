43184fb609aa8421accd4e7871d9f3e0aea2f1a9
dakshinsubhesh <dakshinsubhesh385@gmail.com>
2026-03-12 17:56:42 +0530

    fix: apply code formatting

--- tests/test_security_service.py
+++ tests/test_security_service.py
@@ -1,7 +1,6 @@
 from cryptography.fernet import Fernet
 from app.infrastructure.security_service import SecurityService
 
-
 def test_encrypt_decrypt():
     key = Fernet.generate_key()
     service = SecurityService(key)
