cea6c33709db9edaca545d6aef749ad7fb0d38b6
dakshinsubhesh <dakshinsubhesh385@gmail.com>
2026-03-14 22:00:41 +0530

    style: format tests with black

--- tests/test_security_service.py
+++ tests/test_security_service.py
@@ -1,6 +1,7 @@
 from cryptography.fernet import Fernet
 from app.infrastructure.security_service import SecurityService
 
+
 def test_encrypt_decrypt():
     key = Fernet.generate_key()
     service = SecurityService(key)
